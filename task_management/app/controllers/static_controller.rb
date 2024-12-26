971d7e0dfc7afc309c86fd48fe488f5a24863a49
Anandu <nair.anandu94@gmail.com>
2024-12-26 11:06:59 -0800

    Commit 3

--- task_management/app/controllers/static_controller.rb
+++ task_management/app/controllers/static_controller.rb
@@ -1,6 +1,2 @@
 class StaticController < ApplicationController
-    def index
-      render file: Rails.root.join('public', 'index.html')
-    end
-  end
-  
+end
--- task_management/config/routes.rb
+++ task_management/config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-
-  root 'static#index' # Serve React for the root path
-
-  # Fallback for all other paths (React handles routing)
-  get '*path', to: 'static#index', constraints: ->(req) { !req.xhr? && req.format.html? }
-
- 
   # For tasks-related endpoints
   resources :tasks, only: [:index, :create, :update, :destroy]
 
