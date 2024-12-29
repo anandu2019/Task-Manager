8f23491ac08a300485524ad91d08fa3042588265
anandu2019 <47862653+anandu2019@users.noreply.github.com>
2024-12-29 12:53:24 -0800

    updated logic from backend

--- React/my-app/src/components/OverdueTasks.js
+++ React/my-app/src/components/OverdueTasks.js
@@ -1,31 +1,55 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 class OverdueTasks extends Component {
-    render() {
-        return(
 
-       <div className="task-list-container">
+  state = {
+    overdueTasks: []
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:3000/tasks/overdue_tasks', {
+        // headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        this.setState({ overdueTasks: response.data });
+      })
+      .catch((error) => {
+        console.error('Error fetching overdue tasks:', error);
+        alert('Failed to fetch overdue tasks. Please log in again.');
+      });
+  }
+  render() {
+
+    const { overdueTasks } = this.state;
+
+    return (
+      <div className="task-list-container">
         <h2>Overdue Task List</h2>
-        {this.props.showTaskList && this.props.overdueTasks.length > 0 ? (
-          <table className="task-table">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Description</th>
-                <th>Status</th>
-                <th>Due Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.props.overdueTasks.map((task) => (
-                <tr key={task.id}>
-                  <td>{task.title}</td>
-                  <td>{task.description}</td>
-                  <td>{task.status}</td>
-                  <td>{task.due_date}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+        {overdueTasks && Object.keys(overdueTasks).length > 0 ? (
+          Object.entries(overdueTasks).map(([status, tasks]) => (
+            <div key={status} className="task-group">
+              <h3>{status}</h3>
+              <table className="task-table">
+                <thead>
+                  <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Due Date</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {tasks.map((task) => (
+                    <tr key={task.id}>
+                      <td>{task.title}</td>
+                      <td>{task.description}</td>
+                      <td>{new Date(task.due_date).toLocaleDateString()}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          ))
         ) : (
           <p>No overdue tasks available</p>
         )}
